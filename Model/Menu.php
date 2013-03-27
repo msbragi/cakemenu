eafbb5f0160b8f49e4a587b710991b783585b784
Marco Sbragi <m.sbragi@gmail.com>
2013-03-27 11:45:05 +0100

    Removed where function used getPath where needed
    Signed-off-by: Marco Sbragi <m.sbragi@gmail.com>

--- Model/Menu.php
+++ Model/Menu.php
@@ -5,23 +5,15 @@ App::uses('TreeBehavior', 'Model/Behavior');
 
 class Menu extends CakemenuAppModel {
 
-  public $name = 'Menu';
-  public $useTable = 'cakemenu';
-  public $displayField = 'name';
-  public $actsAs = array('Tree');
-  //The Associations below have been created with all possible keys, those that are not needed can be removed
+	public $name = 'Menu';
+	public $useTable = 'cakemenu';
+	public $displayField = 'name';
+	public $actsAs = array('Tree');
 
-  /**
-   * Function which return single array containing the path to the node.
-   * it uses getpath, but extracts only the id and another field as value
-   * @param integer id of the node
-   * @param string $field could be any column from the menu table
-   */
-  public function where($id, $field='id') {
-    $nodes = $this->getpath(intval($id));
-    return Set::extract($nodes, '/Menu/' . $field);
-  }
+	public $validate = array(
+		'name' => 'notempty',
+		'link' => 'notempty',
+	);
 
 }
-
 ?>
