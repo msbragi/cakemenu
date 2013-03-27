8e3b9cc6e42422f12dff2ce9d21a3f6f99b36d42
Marco Sbragi <m.sbragi@gmail.com>
2013-03-27 11:43:33 +0100

    Management of title field
    Signed-off-by: Marco Sbragi <m.sbragi@gmail.com>

--- Controller/CakemenuController.php
+++ Controller/CakemenuController.php
@@ -6,23 +6,27 @@ App::uses('Cakemenu', 'Cakemenu.Helper/View');
 class CakemenuController extends CakemenuAppController {
 	public $name = 'Cakemenu';
 	public $uses = array('Cakemenu.Menu');
-	public $helpers = array('Cakemenu.Cakemenu');
 
 	public function index() {
-		$menu_list = $this->Menu->generateTreeList(null, null, null, '&nbsp;&nbsp;&nbsp;');
+		$menu_list = $this->Menu->generateTreeList(null, null, null, '<span class="indent">|—</span>');
 		//Get links
 		$links = $this->Menu->find('list', array('fields'=>array('id', 'link')));
-		
+		//Get display
+		$displays = $this->Menu->find('list', array('fields'=>array('id', 'display')));
+		//Get title
+		$titles = $this->Menu->find('list', array('fields'=>array('id', 'title')));
 		$this->set('menu_list', $menu_list);
 		$this->set('links', $links);
+		$this->set('titles', $titles);
+		$this->set('displays', $displays);
 	}
-	
+
 	public function preview($type = null){
 		$this->set('type', $type);
 		$menu = $this->Menu->find('threaded');
 		$this->set('menu', $menu);
 	}
-	
+
 	public function move($id = null, $direction = 'down'){
 		if($direction == 'down'){
 			$this->Menu->moveDown(intval($id));
@@ -31,7 +35,7 @@ class CakemenuController extends CakemenuAppController {
 		}
 		$this->redirect(array('action'=>'index'));
 	}
-	
+
 	public function recover(){
 		$this->Menu->recover($this->Menu);
 		$this->redirect(array('action'=>'index'));
@@ -40,30 +44,31 @@ class CakemenuController extends CakemenuAppController {
 	public function edit($id = null) {
 		if (!empty($this->data)) {
 			if ($this->Menu->save($this->data)) {
-				$this->Session->setFlash(sprintf(__('The %s has been saved'), 'cakemenu'));
+				$this->Session->setFlash(sprintf(__('The %s has been saved'), 'Menu'), 'success');
 				$this->redirect(array('action' => 'index'));
 			} else {
-				$this->Session->setFlash(sprintf(__('The %s could not be saved. Please, try again.'), 'cakemenu'));
+				$this->Session->setFlash(sprintf(__('The %s could not be saved. Please, try again.'), 'Menu'), 'error');
 			}
 		}
 		if (empty($this->data)) {
 			$this->data = $this->Menu->read(null, $id);
 		}
 		$parents = $this->Menu->generateTreeList(null, null, null, '___');
-		$this->set(compact('parentCakemenus', 'parents'));
+		$avalues = array('-999' => 'Always', '1' => 'When not logged','2' => 'When logged');
+		$this->set(compact('parentCakemenus', 'parents', 'avalues'));
 	}
 
 	public function delete($id = null) {
 		if (!$id) {
-			$this->Session->setFlash(sprintf(__('Invalid id for %s'), 'Menu'));
+			$this->Session->setFlash(sprintf(__('Invalid id for %s'), 'Menu'), 'warning');
 			$this->redirect(array('action'=>'index'));
 		}
 		if ($this->Menu->removeFromTree($id)) {
 			$this->Menu->delete($id);
-			$this->Session->setFlash(sprintf(__('%s deleted'), 'Menu'));
+			$this->Session->setFlash(sprintf(__('%s deleted'), 'Menu'), 'success');
 			$this->redirect(array('action'=>'index'));
 		}
-		$this->Session->setFlash(sprintf(__('%s was not deleted'), 'Menu'));
+		$this->Session->setFlash(sprintf(__('%s was not deleted'), 'Menu'), 'error');
 		$this->redirect(array('action' => 'index'));
 	}
 }
--- View/Cakemenu/edit.ctp
+++ View/Cakemenu/edit.ctp
@@ -4,10 +4,12 @@
     <legend><?php printf(__('Edit %s'), __('Cakemenu', true)); ?></legend>
     <?php
     echo $this->Form->input('id');
-    echo $this->Form->input('name');
-    echo $this->Form->input('link');
+    echo $this->Form->input('name', array('type' => 'text'));
+    echo $this->Form->input('link', array('type' => 'text'));
+    echo $this->Form->input('title', array('type' => 'text'));
     echo $this->Form->input('parent_id', array('empty' => true));
-    echo $this->Form->input('icon');
+    echo $this->Form->input('display', array('type' => 'select', 'options' => $avalues, 'empty' => false));
+    echo $this->Form->input('icon', array('type' => 'text'));
     ?>
   </fieldset>
   <?php echo $this->Form->end(__('Submit')); ?>
